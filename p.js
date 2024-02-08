d0b068c78e7c43983908aa9abbc51e6b94d81a8a
Md. Bakhtiar Galib Siyam <106044768+siyam97cse@users.noreply.github.com>
2024-02-08 20:00:02 +0600

    Update p.js

--- p.js
+++ p.js
@@ -2,10 +2,10 @@ function monthlySavings(arr, livingCost){
     let sum = 0;
     for(let i = 0; i < arr.length; i++){
         let amount;
-        if(arr[i] > 3000){
-            amount = arr[i] + (arr[i]*(20/100));
+        if(arr[i] >= 3000){
+            amount = arr[i] - (arr[i]*(20/100));
             
-            console.log('p', amount)
+          
         }
          else {
              amount = arr[i];
@@ -13,12 +13,10 @@ function monthlySavings(arr, livingCost){
        sum  = sum + amount;
     }
     
-    console.log(sum);
-    
-    sum = sum - livingCost;
+    sum = livingCost - sum;
         return sum;
 }
 
-const monthlyEarning = [1000, 2000, 10000];
-const monthlyCost = 5000;
+const monthlyEarning = [1000, 2000, 3000];
+const monthlyCost = 5400;
 console.log(monthlySavings(monthlyEarning, monthlyCost));
